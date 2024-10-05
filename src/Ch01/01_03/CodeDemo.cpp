32029ea16e3f7af45853202c4b3193518199c5bb
disha28172 <disha.s28172@gmail.com>
2024-10-05 12:23:35 +0530

    program 1

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -7,9 +7,9 @@
 
 int main(){
     std::string str;
-    std::cin >> str;
-    std::cout << str;
+    std::cin "my name is">> str;
+    std::cout"hello nice to meet you"<< str;
 
-    std::cout << std::endl << std::endl;
+    std::cout"will you comw with me" << std::endl << std::endl;
     return (0);
 }
